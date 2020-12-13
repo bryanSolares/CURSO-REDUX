b1a6586bb57bdcbd20717956e3f1298874874ce1
solaresbook-linux-mint <solares.josue@outlook.com>
2020-12-12 20:45:04 -0600

    Fin sección 5, y deplegado de aplicación en githubpages

--- Proyectos/03-todoApp/src/app/todos/todo-page/todo-page.component.ts
+++ Proyectos/03-todoApp/src/app/todos/todo-page/todo-page.component.ts
@@ -1,15 +1,22 @@
 import { Component, OnInit } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { AppState } from '../../app.reducer';
+import { toggleAllCompletado, toggleCompletado } from '../todo.actions';
 
 @Component({
   selector: 'app-todo-page',
   templateUrl: './todo-page.component.html',
-  styleUrls: ['./todo-page.component.css']
+  styleUrls: ['./todo-page.component.css'],
 })
 export class TodoPageComponent implements OnInit {
+  completado = false;
 
-  constructor() { }
+  constructor(private store: Store<AppState>) {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  toggleAll() {
+    this.completado = !this.completado;
+    this.store.dispatch(toggleAllCompletado({ completado: this.completado }));
   }
-
 }
